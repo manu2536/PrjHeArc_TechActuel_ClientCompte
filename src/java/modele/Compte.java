3f6c1d86242732cbe121788d86688115b64cca9e
jeremy.wermeill <jeremy.wermeill@EGE-WERMEILLEJ.intra.eiaj.ch>
2015-03-31 11:56:45 +0200

    ajout du numéro de compte

--- src/java/modele/Compte.java
+++ src/java/modele/Compte.java
@@ -2,68 +2,78 @@ package modele;
 
 import java.io.Serializable;
 
-public class Compte implements Serializable{
-    private Integer identifiant;
-    private String  nom;
-    private Float   solde;
-    private Float   taux;
-    
-    public Compte(Integer _identifiant,String _nom,Float _solde,Float _taux){
-        this.identifiant = _identifiant;
-        this.nom = _nom;
-        this.solde = _solde;
-        this.taux = _taux;
-    }
-    
-    public Compte(){
-        this.identifiant = null;
-        this.nom = null;
-        this.solde = null;
-        this.taux = null;
-    }
+public class Compte implements Serializable {
 
-    @Override
-    public String toString(){
-        return String.valueOf(identifiant) + "," + nom + "," + String.valueOf(solde) + "," + String.valueOf(taux);
-    }
-    
-    public void print(){
-        System.out.println(this.toString());
-    }
-    
-    public boolean isNull(){
-        return nom==null && solde==null && taux==null && identifiant==null;
-    }
-    
-    public Integer getIdentifiant() {
-        return identifiant;
-    }
+  private Integer identifiant;
+  private String numero;
+  private String nom;
+  private Float solde;
+  private Float taux;
 
-    public void setIdentifiant(Integer identifiant) {
-        this.identifiant = identifiant;
-    }
+  public Compte(Integer _identifiant, String _nom, Float _solde, Float _taux) {
+    this.identifiant = _identifiant;
+    this.nom = _nom;
+    this.solde = _solde;
+    this.taux = _taux;
+  }
 
-    public String getNom() {
-        return nom;
-    }
+  public Compte() {
+    this.identifiant = null;
+    this.nom = null;
+    this.solde = null;
+    this.taux = null;
+  }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+  @Override
+  public String toString() {
+    return String.valueOf(identifiant) + "," + nom + "," + String.valueOf(solde) + "," + String.valueOf(taux);
+  }
 
-    public Float getSolde() {
-        return solde;
-    }
+  public void print() {
+    System.out.println(this.toString());
+  }
 
-    public void setSolde(Float solde) {
-        this.solde = solde;
-    }
+  public boolean isNull() {
+    return nom == null && solde == null && taux == null && identifiant == null;
+  }
 
-    public Float getTaux() {
-        return taux;
-    }
+  public Integer getIdentifiant() {
+    return identifiant;
+  }
 
-    public void setTaux(Float taux) {
-        this.taux = taux;
-    }
+  public void setIdentifiant(Integer identifiant) {
+    this.identifiant = identifiant;
+  }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public Float getSolde() {
+    return solde;
+  }
+
+  public void setSolde(Float solde) {
+    this.solde = solde;
+  }
+
+  public Float getTaux() {
+    return taux;
+  }
+
+  public void setTaux(Float taux) {
+    this.taux = taux;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
 }
